df6a6f3bdcf45cc4e4ee9f5d2b95ee55bdcafb90
YehonatanKadosh <jacojacoj3@gmail.com>
2021-11-22 14:58:09 +0200

    question.create

--- src/routes/Question.routs.js
+++ src/routes/Question.routs.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Routes } from "react-router";
-import CreateQuestion from "../components/CreateQuestion/CreateQuestion";
+import CreateQuestion from "../components/Question/Question.create";
 import QuestionSearch from "../components/Question/Question.search";
 
 function QuestionRouts() {
